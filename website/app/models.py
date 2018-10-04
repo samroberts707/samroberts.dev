2792e5054a7e2ba88cfa614710af16a5f39e2a2b
Sam Roberts <sam@nucreative.co.uk>
2018-10-04 15:32:49 +0100

    Start working on the models for the site

--- website/app/models.py
+++ website/app/models.py
@@ -1,7 +1,31 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextField
 
 # Create your models here.
-class Jobs(models.Model):
+class Job(models.Model):
     title = models.CharField(max_length=200)
-    # slug = models.
+    slug = models.SlugField(max_length=200, unique=True)
+    object_order = models.PositiveIntegerField(default=0, blank=False, null=False)
+
+    company_name = models.CharField(max_length=200)
+    company_URL = models.URLField()
+
+    start_date = models.CharField(max_length=200)
+    finish_date = models.CharField(max_length=200)
+
+    description = RichTextField()
+
+    def __str__(self):
+        return self.title
+
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super (Job, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Job"
+        verbose_name_plural = "Jobs"
+        ordering = ['object_order']
