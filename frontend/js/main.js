7a78011bdaad86460ac2c74bfb710fbb557abed5
Sam Roberts <samroberts707@gmail.com>
2018-10-10 11:48:34 +0100

    Cleanup some JS code

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,11 +1,11 @@
 $(window).on("load", function() {
-  console.log('Window Loaded!');
+  // This is for when I implement a loading screen.
 });
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
+    $('nav').addClass("shrink");
   } else {
-    $('nav').removeClass('shrink');
+    $('nav').removeClass("shrink");
   }
 });
--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,13 +1,13 @@
 $(window).on("load", function() {
-  console.log('Window Loaded!');
+  console.log("Window Loaded!");
 });
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
+    $('nav').addClass("shrink");
   } else {
-    $('nav').removeClass('shrink');
+    $('nav').removeClass("shrink");
   }
 });
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKHdpbmRvdykub24oXCJsb2FkXCIsIGZ1bmN0aW9uKCkge1xuICBjb25zb2xlLmxvZygnV2luZG93IExvYWRlZCEnKTtcbn0pO1xuXG4kKHdpbmRvdykuc2Nyb2xsKGZ1bmN0aW9uKCkge1xuICBpZiAoJChkb2N1bWVudCkuc2Nyb2xsVG9wKCkgPiA1MCkge1xuICAgICQoJ25hdicpLmFkZENsYXNzKCdzaHJpbmsnKTtcbiAgfSBlbHNlIHtcbiAgICAkKCduYXYnKS5yZW1vdmVDbGFzcygnc2hyaW5rJyk7XG4gIH1cbn0pO1xuIl19
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyIkKHdpbmRvdykub24oXCJsb2FkXCIsIGZ1bmN0aW9uKCkge1xuICBjb25zb2xlLmxvZyhcIldpbmRvdyBMb2FkZWQhXCIpO1xufSk7XG5cbiQod2luZG93KS5zY3JvbGwoZnVuY3Rpb24oKSB7XG4gIGlmICgkKGRvY3VtZW50KS5zY3JvbGxUb3AoKSA+IDUwKSB7XG4gICAgJCgnbmF2JykuYWRkQ2xhc3MoXCJzaHJpbmtcIik7XG4gIH0gZWxzZSB7XG4gICAgJCgnbmF2JykucmVtb3ZlQ2xhc3MoXCJzaHJpbmtcIik7XG4gIH1cbn0pO1xuIl19
