5a92d89538868f72e656cb4ab3b53ba35660877c
Sam Roberts <samroberts707@gmail.com>
2019-07-16 16:52:23 +0100

    Start working on a new design for the project section

--- src/components/Web-Projects.vue
+++ src/components/Web-Projects.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="web-projects">
-    <div class="project" :class="{'open': project.opened}" v-for="project in projects" v-bind:key="project.title" :id="project.title">
-      <div class="heading">
-        <img :src="project.background" class="project-background"/>
-        <div class="heading-overlay">
-          <h1>{{ project.title }}</h1>
-        </div>
-      </div>
+    <div class="project" v-for="project in projects" v-bind:key="project.title" :id="project.title">
       <div class="content">
-        <p>{{ project.content }}</p>
+        <h2>{{ project.title }}</h2>
+        <h4>{{ project.url }}</h4>
+        <ul>
+          <li v-for="tech in project.technology" v-bind:key="tech">{{tech}}</li>
+        </ul>
+        <p>{{ project.content }}
+        </p>
+      </div>
+      <div class="mockup">
+        <img :src="project.mockup" :alt="project.title"/>
       </div>
     </div>
   </div>
@@ -22,16 +25,22 @@ export default {
         {
           'title': 'Project 1',
           'url': 'https://google.com',
-          'background': 'https://www.geraldedelman.com/static/images/hero/landing-section.jpg',
-          'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus nisl eget ipsum dapibus scelerisque. Pellentesque sodales sem tincidunt, dictum eros ac, tempus ipsum.',
-          'opened': false
+          'mockup': 'https://gm.imgix.net/https%3A%2F%2Foriginalmockups.com%2Fmedia%2Fpages%2Fstock%2Foriginalmockups%2Fdevices%2Fiphone-xs-max-mockup-06%2F1516525845-1562975523%2F08-iphone-xs-max-mockup-06-poster.jpg?auto=format&fit=max&ixlib=php-2.3.0&q=90&w=1440&s=bb418c6188faadeb564b9a746643b63b',
+          'technology': [
+            'Django',
+            'Bootstrap 4'
+          ],
+          'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus nisl eget ipsum dapibus scelerisque. Pellentesque sodales sem tincidunt, dictum eros ac, tempus ipsum.'
         },
         {
           'title': 'Project 2',
           'url': 'https://google.com',
-          'background': 'https://elementscommunications.com/media/filer_public/1e/ad/1ead472a-f0f5-4ed7-b806-d87c7be0b2e8/header-elements.jpg',
-          'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus nisl eget ipsum dapibus scelerisque. Pellentesque sodales sem tincidunt, dictum eros ac, tempus ipsum.',
-          'opened': false
+          'mockup': 'https://gm.imgix.net/https%3A%2F%2Foriginalmockups.com%2Fmedia%2Fpages%2Fstock%2Foriginalmockups%2Fdevices%2Fiphone-xs-max-mockup-06%2F1516525845-1562975523%2F08-iphone-xs-max-mockup-06-poster.jpg?auto=format&fit=max&ixlib=php-2.3.0&q=90&w=1440&s=bb418c6188faadeb564b9a746643b63b',
+          'technology': [
+            'Django CMS',
+            'Bootstrap 4'
+          ],
+          'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dapibus nisl eget ipsum dapibus scelerisque. Pellentesque sodales sem tincidunt, dictum eros ac, tempus ipsum.'
         }
       ]
     }
@@ -43,94 +52,24 @@ export default {
 
 <style lang="scss" scoped>
   div#web-projects {
+    display: block;
+    width: auto;
     position: relative;
-    display: grid;
-    grid-template-columns: 50% 50%;
-    width: 100%;
+    padding: 0 10%;
     div.project {
-      position: absolute;
-      width: 50%;
-      height: auto;
-      min-height: 100%;
-      transition: all 0.5s ease-in-out;
-      div.heading {
-        position: relative;
-        width: 100%;
-        height: 50vw;
-        img.project-background {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-        }
-        div.heading-overlay {
-          position: absolute;
-          top: 0;
-          width: 100%;
-          height: 100%;
-          background-color: rgba(0, 0, 0, 0.75);
-          cursor: pointer;
-          h1 {
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            font-size: 3vw;
-            color: #FFFFFF;
-            white-space: nowrap;
-          }
-        }
-      }
+      position: relative;
+      display: grid;
+      grid-template-columns: 40% 60%;
+      margin-bottom: 50px;
       div.content {
-        position: relative;
-        width: calc(100% - 60px);
-        height: auto;
-        max-height: 0;
-        padding: 0;
-        overflow: hidden;
-        transition: max-height 0.5s ease-in-out, padding 0.5s ease-in-out;
+        padding: 30px 30px 30px 0;
       }
-      &.open {
-        left: 15%;
-        top: 50px;
-        width: 70%;
-        div.heading {
-          div.heading-overlay {
-            left: 0;
-          }
-        }
-        div.content {
-          padding: 30px;
-          max-height: 300px;
-        }
-      }
-      &:first-child {
-        left: 0;
-        top: 0;
-        div.heading {
-          div.heading-overlay {
-            left: -100%;
-            transition: left 0.5s ease-in-out;
-          }
-          &:hover {
-            div.heading-overlay {
-              left: 0;
-            }
-          }
-        }
-      }
-      &:nth-child(2) {
-        right: 0;
-        top: 0;
-        div.heading {
-          div.heading-overlay {
-            right: -100%;
-            transition: right 0.5s ease-in-out;
-          }
-          &:hover {
-            div.heading-overlay {
-              right: 0;
-            }
-          }
+      div.mockup {
+        display: block;
+        img {
+          display: block;
+          width: 100%;
+          height: auto;
         }
       }
     }
