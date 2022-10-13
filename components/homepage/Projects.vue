603855f856b090c986ac2b0e56a3f4b3f1237297
Sam Roberts <samroberts707@gmail.com>
2022-10-13 13:20:29 +0100

    Add extra projects

--- components/homepage/Projects.vue
+++ components/homepage/Projects.vue
@@ -10,7 +10,7 @@
       data-cursor-text="Link"
     >
       <picture>
-        <img class="project-background" :alt="project.title" :src="'/projects/' + project.slug + '.jpg'">
+        <img class="project-background" :alt="project.title" :src="'/projects/' + project.img">
       </picture>
       <h2>{{ project.title }}</h2>
     </a>
@@ -23,23 +23,63 @@ export default {
     return {
       projects: [
         {
-          title: 'Piiq',
-          slug: 'piiq',
-          url: 'https://piiqrp.com'
+          title: 'Advance Consultancy',
+          slug: 'advance-consultancy',
+          img: 'advance-consultancy.gif',
+          url: 'https://www.advance-consultancy.com'
+        },
+        {
+          title: 'Known Media',
+          slug: 'known-media',
+          img: 'known-media.gif',
+          url: 'https://known-media.com'
+        },
+        {
+          title: 'Xenia',
+          slug: 'xenia',
+          img: 'xenia.jpg',
+          url: 'https://xeniabroking.com'
+        },
+        {
+          title: 'Wellbeing of Women',
+          slug: 'wellbeing-of-women',
+          img: 'wellbeing-of-women.jpg',
+          url: 'https://wellbeingofwomen.org.uk'
+        },
+        {
+          title: 'Dugdale Centre',
+          slug: 'dugdale',
+          img: 'dugdale.jpg',
+          url: 'https://www.dugdalecentre.co.uk'
+        },
+        {
+          title: 'Youth Engagement Toolkit',
+          slug: 'youth-engagement-toolkit',
+          img: 'youth-engagement-toolkit.jpg',
+          url: 'https://voiceopportunitypower.com'
         },
         {
           title: 'Gerald Edelman',
           slug: 'gerald-edelman',
+          img: 'gerald-edelman.jpg',
           url: 'https://www.geraldedelman.com'
+        },
+        {
+          title: 'Piiq',
+          slug: 'piiq',
+          img: 'piiq.jpg',
+          url: 'https://piiqrp.com'
         },
         {
           title: 'NU Creative',
           slug: 'nu-creative',
+          img: 'nu-creative.jpg',
           url: 'https://www.nucreative.co.uk'
         },
         {
           title: 'CoderDojo Rise',
           slug: 'coderdojo-rise',
+          img: 'coderdojo-rise.jpg',
           url: 'https://coderdojorise.com/'
         }
       ]
