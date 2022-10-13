af2e7cf04a5e0846c55e38f5ab5809c3cff5c4df
Sam Roberts <samroberts707@gmail.com>
2022-10-13 11:24:49 +0100

    Add Contact component

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,21 @@
     <Landing />
     <Timeline />
     <Projects />
+    <Contact />
   </div>
 </template>
 <script>
 import Landing from '@/components/homepage/Landing.vue'
 import Timeline from '@/components/homepage/Timeline.vue'
 import Projects from '@/components/homepage/Projects.vue'
+import Contact from '@/components/homepage/Contact.vue'
 export default {
   name: 'HomePage',
   components: {
     Landing,
     Timeline,
-    Projects
+    Projects,
+    Contact
   }
 }
 </script>
