1823cfbaf75566404c8c0dbb95b8f2d093fea359
Sam Roberts <samroberts707@gmail.com>
2024-12-11 16:16:30 +0000

    Build a base nav

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,11 +1,35 @@
+"use client";
+
+import { useState } from 'react';
+import Link from 'next/link';
+
 export default function Navigation() {
+
+    const [navOpen, setNavOpen] = useState(false);
+
+    const handleNavToggle = () => {
+        setNavOpen(!navOpen);
+    }
+
     return (
-        <nav id="Navigation">
-            <div className="hamburger">
-                <div className="bar one" />
-                <div className="bar two" />
-                <div className="bar three" />
+    <nav id="Navigation" className={navOpen ? 'open' : ''}>
+        <div className="hamburger" onClick={handleNavToggle}>
+            <div className="bar one" />
+            <div className="bar two" />
+            <div className="bar three" />
+        </div>
+        <div className="menu">
+            <h6>Menu</h6>
+            <div className='items'>
+                <h4><Link href="/">Home</Link></h4>
+                <h4><Link href="/">About</Link></h4>
+                <h4><Link href="/">Projects</Link></h4>
+                <h4><Link href="/">Contact</Link></h4>
             </div>
-        </nav>
+            <div className='links'>
+
+            </div>
+        </div>
+    </nav>
     )
 }
