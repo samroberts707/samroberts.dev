7657d668be3d85e7479fdd99333320234881aa58
Sam Roberts <samroberts707@gmail.com>
2024-12-11 14:20:19 +0000

    Make alterations to the global typography

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -5,13 +5,13 @@ export default function TypographyPage() {
     return (
         <div className="Typography">
             <div className="container">
-                <h1>Heading 1</h1>
-                <h2>Heading 2</h2>
-                <h3>Heading 3</h3>
-                <h4>Heading 4</h4>
-                <h5>Heading 5</h5>
-                <h6>Heading 6</h6>
-                <p>Lorem Ipsum</p>
+                <h1>Heading 1 / <strong>Heading 1</strong></h1>
+                <h2>Heading 2 / <strong>Heading 2</strong></h2>
+                <h3>Heading 3 / <strong>Heading 3</strong></h3>
+                <h4>Heading 4 / <strong>Heading 4</strong></h4>
+                <h5>Heading 5 / <strong>Heading 5</strong></h5>
+                <h6>Heading 6 / <strong>Heading 6</strong></h6>
+                <p>Lorem Ipsum / <strong>Lorem Ipsum</strong></p>
                 <p style={{width: "60%"}}>Nunc maximus porta purus eu consectetur. Donec maximus leo at arcu consequat rhoncus. Ut luctus tincidunt quam in fringilla. Ut ut arcu suscipit, mattis purus et, ornare nulla. Mauris enim libero, convallis et nibh ut, fringilla egestas erat. Nunc et nisi non elit elementum tristique ac a libero. Fusce sed scelerisque odio.</p>
                 <p><Link href="/">Home link text</Link></p>
                 <label style={{alignSelf: 'flex-start'}}>Label</label>
