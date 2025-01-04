e1bc5b253f93688e578ff56ed91e430f72fb26a3
Sam Robets <samroberts707@gmail.com>
2025-01-04 19:28:44 +0000

    Add scroll state to nav

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,18 +1,44 @@
 "use client";
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navigation() {
 
     const [navOpen, setNavOpen] = useState(false);
+    const [scrollNav, setScrollNav] = useState(false);
 
     const handleNavToggle = () => {
         setNavOpen(!navOpen);
     }
 
+
+    const handleScroll = () => {
+        if(scrollY >= 120) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    });
+
     return (
-    <nav id="Navigation" className={navOpen ? 'open' : ''}>
+    <nav id="Navigation" className={`
+            ${navOpen ? 'open' : ''}
+            ${scrollNav ? 'scrolled' : ''}`}>
+        <Link href="/">
+            <Image
+                className="logo"
+                src={'/images/logo-xanthous.svg'}
+                width={150}
+                height={80}
+                alt='Sam Roberts'
+            />
+        </Link>
         <div className="hamburger" onClick={handleNavToggle} data-cursor-text="Menu">
             <div className="bar one" />
             <div className="bar two" />
