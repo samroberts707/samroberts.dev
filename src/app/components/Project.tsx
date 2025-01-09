8cd7cacb32f57733a12fadb76379f2563ca4e880
Sam Roberts <samroberts707@gmail.com>
2025-01-09 16:50:41 +0000

    Add some tester Case Study content

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -2,13 +2,27 @@
 
 import Link from "next/link";
 import Image from "next/image";
+// import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+// import { useGSAP } from '@gsap/react';
 
-import "@/app/styles/components/Footer.scss";
+// import "@/app/styles/components/Footer.scss";
+
+// if (typeof window !== 'undefined') {
+//     gsap.registerPlugin(ScrollTrigger, useGSAP);
+// }
 
 export default function Project(
     {title, url, image, summary, type}:
     {title:string, url:string, image:string, summary: string, type: string}
 ) {
+
+    // useGSAP(
+    //     () => {
+    //         const projectCard = document.querySelector
+    //     }
+    // )
+
     return (
         <div className="Project">
             <h5>{type}</h5>
